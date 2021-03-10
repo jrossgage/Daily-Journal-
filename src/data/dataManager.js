0edf00b0d639fb81ab472c4cccd23812f8b37860
Joel <jrossgage@gmail.com>
2021-03-10 12:47:44 -0600

    first commit. Includes Html with fieldsets and journal js components.

---
+++ src/data/dataManager.js
@@ -0,0 +1,11 @@
+// export const getJournalEntry = () => {
+
+//     return fetch("http://localhost:8088/users")
+//     .then(response => response.json())
+// };
+
+// export const getConcepts = () => {
+
+//     return fetch("http://localhost:8088/posts")
+//     .then(response => response.json())
+// };
